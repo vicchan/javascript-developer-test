efebe5f985952c89a5e785e7e2f7411275964773
vicchan <vcnc86@gmail.com>
2025-10-01 09:12:05 +1000

    feat: implement getArnieQuotes function

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,17 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const quotePromises = urls.map(async (url) => {
+    const { status, body } = await httpGet(url);
+    const { message } = JSON.parse(body);
+
+    if (status === 200) {
+      return { 'Arnie Quote': message };
+    }
+    return { 'FAILURE': message };
+  });
+
+  return Promise.all(quotePromises);
 };
 
 module.exports = {
